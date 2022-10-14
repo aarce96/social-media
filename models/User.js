a6ea5f53ce666f08100d335a859f9254c0d6bd94
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-10-13 20:28:57 -0400

    created routes, controllers and updated models

--- models/User.js
+++ models/User.js
@@ -1,36 +1,47 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     userName: {
-        type: string,
-        unique: true,
-        required: true,
-        trim: true
+      type: string,
+      unique: true,
+      required: true,
+      trim: true,
     },
     email: {
-        type: string,
-        require: true,
-        unique: true,
-        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, 'Must match email address']
+      type: string,
+      require: true,
+      unique: true,
+      match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, "Must match email address"],
     },
-    thoughts: [{ref: 'Thought', type: Schema.Types.ObjectId}],
-    friends: [{ref: 'User', type: Schema.Types.ObjectId,}]
-},
-{
+    thoughts: [
+      {
+        ref: "Thought",
+        type: Schema.Types.ObjectId,
+      },
+    ],
+    friends: [
+      {
+        ref: "User",
+        type: Schema.Types.ObjectId,
+      },
+    ],
+  },
+  {
     toJSON: {
-        virtuals: true,
-        getters: true
+      virtuals: true,
+      getters: true,
     },
-    id: false
-}
+    id: false,
+  }
 );
 
-UserSchema.virtual('friendCount').get(function() {
-    return this.friends.length
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-// create the User model using the PizzaSchema
-const User = model('User', UserSchema)
+// create the User model using the UserSchema
+const User = model("User", UserSchema);
 
 // export the User model
 module.exports = User;
